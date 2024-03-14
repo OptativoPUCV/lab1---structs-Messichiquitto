cd79f77bbaa57f4adef3e14d7ecd35a40faf67b3
Your Name <you@example.com>
2024-03-14 21:14:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -52,9 +52,9 @@ int *filterEvenNumbers(int arr[], int size, int *newSize)
   *newSize = 0;
   for(int i = 0; i < size; i++)
     {
+      newArr = realloc(newArr, sizeof(int) * (*newSize + 1));
       if(arr[i] % 2 == 0)
       {
-        newArr = realloc(newArr, sizeof(int) * (*newSize + 1));
         newArr[*newSize] = arr[i];
         (*newSize)++;
       }
